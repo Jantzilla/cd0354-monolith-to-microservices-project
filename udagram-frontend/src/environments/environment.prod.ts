ca9f8af2ddd8095979dbe96c4c866490ca0cc610
Jantz <jantzillac.93@gmail.com>
2022-04-20 11:09:07 -0500

    Change apiHost in environment files

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://abf65415beaf345baaa0c1bfcc13c936-904464125.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
